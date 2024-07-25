0b5dcbfa63d550b7c11aae5e36823e3467fa92fa
ankitjhagithub21 <ajha45075@gmail.com>
2024-07-25 15:06:27 +0530

    added artist details

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,19 +1,48 @@
-import Song from "./Song"
+import React, { useRef } from 'react';
+import Song from "./Song";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
-const Slider = ({songs}) => {
+const Slider = ({ songs }) => {
+  const scrollContainerRef = useRef(null);
+
+  const scrollLeft = () => {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollBy({ left: -200, behavior: 'smooth' });
+    }
+  };
+
+  const scrollRight = () => {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollBy({ left: 200, behavior: 'smooth' });
+    }
+  };
 
   return (
-    <div className='flex flex-col items-center justify-center my-24 w-full'>
-     <div className='grid grid-rows-2 grid-flow-col-dense  overflow-x-scroll lg:w-[80vw] w-full'>
-     {
-        songs.map((song)=>{
-            return  <Song key={song.id} song={song}/>
-        })
-     }
-     </div>
-     
+    <div className='flex items-center gap-5 justify-center my-24 w-full'>
+      <FaArrowLeft 
+      size={20}
+        className="cursor-pointer md:block hidden"
+        color='gray' 
+        onClick={scrollLeft} 
+      />
+      <div 
+        className='grid grid-rows-2 grid-flow-col-dense overflow-x-scroll lg:w-[70vw] w-full'
+        ref={scrollContainerRef}
+      >
+        {
+          songs.map((song) => {
+            return <Song key={song.id} song={song} />;
+          })
+        }
+      </div>
+      <FaArrowRight 
+      size={20}
+        className="cursor-pointer md:block hidden" 
+        color="gray" 
+        onClick={scrollRight} 
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Slider
+export default Slider;
--- src/components/SongDetails.jsx
+++ src/components/SongDetails.jsx
@@ -2,13 +2,36 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 
 const SongDetails = () => {
-    const {currSong} = useSelector(state=>state.app)
+  const { currSong } = useSelector(state => state.app)
+  
   return (
-    <div className='lg:w-1/2 mx-auto my-24 text-center'>
-      <img src={currSong?.image[2].url} alt="" className='rounded-full mx-auto' width={200} />
-      <h2 className='text-3xl font-bold mt-5'>{currSong?.name}</h2>
-      {/* <p className='mt-2 text-lg'>{currSong?.artists.primary[0].name}</p> */}
+  <section className='py-24 px-4'>
+      <div className='container mx-auto mb-5  flex flex-wrap'>
+      <div className='md:w-1/2 w-full'>
+        <img src={currSong?.image[2].url}  alt={currSong?.name}  className='rounded-lg mx-auto' />
+      </div>
+      <div>
+        <h2 className='text-2xl font-bold mt-5'>{currSong?.name}</h2>
+        <p className='mt-2 text-lg'>Year : {currSong?.year}</p>
+        <p>Plays : {currSong?.playCount}</p>
+      </div>
+   <div className='w-full overflow-x-scroll'>
+   <h2 className='text-2xl my-5 font-bold'>Artists</h2>
+    <div className='flex items-center gap-2 overflow-x-scroll'>
+      {
+        currSong.artists.primary.map((artist)=>{
+          return <div className='flex flex-col gap-1 items-center' key={artist.id}>
+            
+           <img src={artist.image[2].url} alt={artist.name} className='min-w-32 w-32 rounded-full object-contain' />
+           <p>{artist.name}</p>
+          </div>
+        })
+      }
     </div>
+   </div>
+    </div>
+   
+  </section>
   )
 }
 
