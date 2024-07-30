2d5d16be590f7b5aeb54436c1ee1159f2b566c63
ankitjhagithub21 <ajha45075@gmail.com>
2024-07-30 19:36:28 +0530

    added automatically play

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,25 @@
+import { useState } from 'react';
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
-import { useSelector } from 'react-redux';
-
+import { useDispatch, useSelector } from 'react-redux';
+import { setCurrSong } from '../app/appSlice';
+import { MdSkipNext, MdSkipPrevious } from "react-icons/md";
 const Player = () => {
-  const { currSong } = useSelector(state => state.app)
-
+  const { currSong,songs } = useSelector(state => state.app)
+  const [index,setIndex] = useState(0)
+  const dispatch = useDispatch()
+  const prevSong = () =>{
+    if(index>1){
+      setIndex(index-1)
+    dispatch(setCurrSong(songs[index]))
+    }
+  }
+  const nextSong = () =>{
+   if(index<songs.length){
+    setIndex(index+1)
+    dispatch(setCurrSong(songs[index]))
+   }
+  }
   if (!currSong) return
   return (
     <div className='fixed bottom-0 w-full z-10 flex flex-col bg-gray-100'>
@@ -13,8 +28,18 @@ const Player = () => {
          autoPlay={true}
         src={currSong.downloadUrl}
         showJumpControls={false}
-       
+      onEnded={nextSong}  
       />
+      <div className='flex items-center justify-center text-4xl gap-5 '>
+      <button onClick={prevSong} >
+        <MdSkipPrevious/>
+      </button>
+     
+      <button onClick={nextSong} >
+        <MdSkipNext/>
+      </button>
+      </div>
+    
      
     </div>
   )
