49fb89ccf5073fc096f5493f1bba0486b1561340
ankitjhagithub21 <ajha45075@gmail.com>
2024-07-30 19:38:03 +0530

    fix position

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -5,42 +5,42 @@ import { useDispatch, useSelector } from 'react-redux';
 import { setCurrSong } from '../app/appSlice';
 import { MdSkipNext, MdSkipPrevious } from "react-icons/md";
 const Player = () => {
-  const { currSong,songs } = useSelector(state => state.app)
-  const [index,setIndex] = useState(0)
+  const { currSong, songs } = useSelector(state => state.app)
+  const [index, setIndex] = useState(0)
   const dispatch = useDispatch()
-  const prevSong = () =>{
-    if(index>1){
-      setIndex(index-1)
-    dispatch(setCurrSong(songs[index]))
+  const prevSong = () => {
+    if (index > 1) {
+      setIndex(index - 1)
+      dispatch(setCurrSong(songs[index]))
     }
   }
-  const nextSong = () =>{
-   if(index<songs.length){
-    setIndex(index+1)
-    dispatch(setCurrSong(songs[index]))
-   }
+  const nextSong = () => {
+    if (index < songs.length) {
+      setIndex(index + 1)
+      dispatch(setCurrSong(songs[index]))
+    }
   }
   if (!currSong) return
   return (
     <div className='fixed bottom-0 w-full z-10 flex flex-col bg-gray-100'>
 
       <AudioPlayer
-         autoPlay={true}
+        autoPlay={true}
         src={currSong.downloadUrl}
         showJumpControls={false}
-      onEnded={nextSong}  
+        onEnded={nextSong}
       />
-      <div className='flex items-center justify-center text-4xl gap-5 '>
-      <button onClick={prevSong} >
-        <MdSkipPrevious/>
-      </button>
-     
-      <button onClick={nextSong} >
-        <MdSkipNext/>
-      </button>
+      <div className='flex items-center justify-center text-4xl gap-10 text-gray-500 fixed w-full bottom-3'>
+        <button onClick={prevSong} >
+          <MdSkipPrevious />
+        </button>
+
+        <button onClick={nextSong} >
+          <MdSkipNext />
+        </button>
       </div>
-    
-     
+
+
     </div>
   )
 }
